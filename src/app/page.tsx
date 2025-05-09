e91fcdc8fd840526c3c4366bacba6aae3c9d28ea
Rylan Graham <RylanGraham02@gmail.com>
2025-05-09 22:18:30 +0000

    inbetween the header and the how it works section can you add the app name rauxa, and then its definition from catalan to english. Should be a cool section. do some animation or styling. be creative!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+
 import Header from '@/components/sections/Header';
 import HeroSection from '@/components/sections/HeroSection';
+import RauxaDefinitionSection from '@/components/sections/RauxaDefinitionSection';
 import HowItWorksSection from '@/components/sections/HowItWorksSection';
 import ScreenshotsCarousel from '@/components/sections/ScreenshotsCarousel';
 import TestimonialsSection from '@/components/sections/TestimonialsSection';
@@ -8,10 +10,11 @@ import Footer from '@/components/sections/Footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-grow">
         <HeroSection />
+        <RauxaDefinitionSection />
         <HowItWorksSection />
         <ScreenshotsCarousel />
         <TestimonialsSection />
